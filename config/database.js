659ce1fa42c3a0adc48dd3793c3e142cec993914
amir ghedira <amirghedira06@gmail.com>
2020-05-18 23:48:30 +0100

    V1.1

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 
-var db = new Sequelize('locationapp', 'root', '96901171amir', {
-    host: 'localhost',
+var db = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+    host: process.env.HOST,
     dialect: 'mysql'
 });
 
